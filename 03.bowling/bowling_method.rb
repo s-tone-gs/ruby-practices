519664bf62953e0c7b312dade4bf55bafb3c4cbf
shoma <s.tone@genbasupport.com>
2025-08-01 09:54:19 +0900

    余計な空白と冗長な構文を修正

--- 03.bowling/bowling_method.rb
+++ 03.bowling/bowling_method.rb
@@ -1,6 +1,6 @@
 def strings_to_numbers(scores)
   converted_array = []
-  scores.each do |score| 
+  scores.each do |score|
     if score == 'X'
       converted_array << 10
       converted_array << 0
@@ -13,36 +13,34 @@ end
 
 def format_score(raw_scores)
   scores = raw_scores.split(',')
-  integer_scores = strings_to_numbers(scores) 
+  integer_scores = strings_to_numbers(scores)
   # 10フレーム目でストライクかスペアを出すとframesの要素数が10以上になる。こうすることで10フレーム目のストライク、スペアの得点加算も通常と同じ処理で済むのでこの形にしてある
-  formatted_scores = integer_scores.each_slice(2).to_a
+  integer_scores.each_slice(2).to_a
 end
 
 def strike?(frame)
-  frame[0] == 10 ? true : false
+  frame[0] == 10
 end
 
 def spare?(frame)
-  frame[0] + frame[1] == 10 ? true : false
+  frame[0] + frame[1] == 10
 end
 
-def add_strike_bonus (index, frames)
-  strike_bonus = 0
+def calc_strike_bonus(index, frames)
   next_index = index + 1
   next_frame = frames[next_index]
   if strike? next_frame
     after_next_frame = frames[next_index + 1]
-    strike_bonus += next_frame[0] +  after_next_frame[0]
-  else 
-    strike_bonus += next_frame[0] + next_frame[1]
+    next_frame[0] + after_next_frame[0]
+  else
+    next_frame[0] + next_frame[1]
   end
 end
-    
-def add_spare_bonus (index, frames)
-  spare_bonus = 0
+
+def calc_spare_bonus(index, frames)
   next_index = index + 1
   next_frame = frames[next_index]
-  spare_bonus += next_frame[0] 
+  next_frame[0]
 end
 
 def sum_score(frames)
@@ -52,9 +50,9 @@ def sum_score(frames)
     frame = frames[index]
     total_score += frame[0] + frame[1]
     if strike?(frame)
-      total_score += add_strike_bonus(index, frames)
+      total_score += calc_strike_bonus(index, frames)
     elsif spare?(frame)
-      total_score += add_spare_bonus(index, frames)
+      total_score += calc_spare_bonus(index, frames)
     end
   end
   total_score
