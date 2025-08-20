195b305a58ee638075f59d95c07bef4765826107
shoma <s.tone@genbasupport.com>
2025-08-20 15:28:04 +0900

    ファイルの種類で文字の色が変わるように修正

--- 04.ls/file.rb
+++ 04.ls/file.rb
@@ -2,16 +2,29 @@
 
 module LS
   class File
-    def initialize(name, file_state_instance)
+    def initialize(name, path, file_state_instance)
       @name = name
+      @path = path
       # 今後オプションで機能を拡張する際に使用する予定
       @file_state_instance = file_state_instance
     end
 
-    attr_reader :name
+    attr_reader :name, :path
 
     def directory?
       @file_state_instance.directory?
+    end
+
+    def symlink?
+      @file_state_instance.symlink?
+    end
+
+    def executable?
+      @file_state_instance.executable?
+    end
+
+    def world_writable?
+      @file_state_instance.world_writable?
     end
   end
 end
--- 04.ls/ls_method.rb
+++ 04.ls/ls_method.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
 
 require_relative 'file'
+require 'debug'
 
 NUMBER_OF_COLUMNS = 3
+COLORS = {
+  directory: '34m',
+  symlink: '96m',
+  broken: '91m',
+  exe: '92m',
+  file: '37m'
+}.freeze
 
 def exit_if_not_exist(path)
   puts "ls: cannot access '#{path}': No such file or directory"
@@ -21,7 +29,8 @@ end
 
 def get_files(path)
   Dir.glob('*', base: path).map do |file_name|
-    LS::File.new(file_name, File::Stat.new(path + file_name))
+    file_path = path + file_name
+    LS::File.new(file_name, file_path, File.lstat(file_path))
   end
 end
 
@@ -42,12 +51,28 @@ def calculation_output_width(build_files)
   max_length + 2
 end
 
+def prepare_output(file, width)
+  file_type = if file.directory?
+                :directory
+              elsif file.symlink?
+                if File.exist?(file.path)
+                  :symlink
+                else
+                  :broken
+                end
+              elsif file.executable?
+                :exe
+              else
+                :file
+              end
+  "\e[#{COLORS[file_type]}#{file.name.ljust(width)}\e[0m"
+end
+
 def print_files(build_files)
   output_width = calculation_output_width(build_files)
   build_files.each do |files|
     files.compact.each do |file|
-      output = file.name.ljust(output_width)
-      print file.directory? ? "\e[34m#{output}\e[0m" : output
+      print prepare_output(file, output_width)
     end
     puts ''
   end
