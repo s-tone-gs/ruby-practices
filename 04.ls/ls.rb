55bf2c1d6f0919209a9504f649415f1dee4465ce
shoma <s.tone@genbasupport.com>
2025-08-28 09:43:53 +0900

    インデントを調整

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -5,13 +5,13 @@ require_relative 'ls_method'
 def main
   option_and_path(ARGV) => { all: all, reverse: reverse, paths: paths }
   target_path = if paths[0].nil?
-           './'
-         else
-           exit_if_not_exist(paths[0]) unless FileTest.exist?(paths[0])
-           exit_file_is_true(paths[0]) if FileTest.file?(paths[0])
-           # ディレクトリの記述が/で終わっているかいないかで分岐
-           %r{\S*/$}.match?(paths[0]) ? paths[0] : "#{paths[0]}/"
-         end
+                  './'
+                else
+                  exit_if_not_exist(paths[0]) unless FileTest.exist?(paths[0])
+                  exit_file_is_true(paths[0]) if FileTest.file?(paths[0])
+                  # ディレクトリの記述が/で終わっているかいないかで分岐
+                  %r{\S*/$}.match?(paths[0]) ? paths[0] : "#{paths[0]}/"
+                end
 
   files = get_files(target_path, all)
   exit_if_file_not_found if files.empty?
