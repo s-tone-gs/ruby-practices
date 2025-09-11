f16378fbf256b4fab9a391dbf6567b7d13338994
shoma <s.tone@genbasupport.com>
2025-09-11 10:12:05 +0900

    rubocopで修正

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -19,35 +19,33 @@ def get_option_and_paths(arguments)
 end
 
 def build_stdin_output
-  input = STDIN.read
-  outputs = {
+  input = $stdin.read
+  {
     line: input.lines.count,
     word: input.split.count,
-    byte: input.size,
+    byte: input.size
   }
 end
 
 def build_file_output(path)
-  begin
-    outputs = {
-      line: File.read(path).lines.count,
-      word: File.read(path).split.count,
-      byte: File.size(path),
-      name: path,
-    }
-  rescue Errno::ENOTDIR
-    outputs = {
-      error: "wc: #{path}: Not a directory",
-    }
-  rescue Errno::ENOENT
-    outputs = {
-      error: "wc: #{path}: No such file or directory",
-    }
-  end
+  {
+    line: File.read(path).lines.count,
+    word: File.read(path).split.count,
+    byte: File.size(path),
+    name: path
+  }
+rescue Errno::ENOTDIR
+  {
+    error: "wc: #{path}: Not a directory"
+  }
+rescue Errno::ENOENT
+  {
+    error: "wc: #{path}: No such file or directory"
+  }
 end
 
 def build_directory_output(path)
-  outputs = {
+  {
     message: "wc: #{path}: Is a directory\n",
     line: 0,
     word: 0,
@@ -58,6 +56,7 @@ end
 
 def calculate_total(totals, outputs)
   return totals if outputs.key?(:error)
+
   totals[:line] += outputs[:line]
   totals[:word] += outputs[:word]
   totals[:byte] += outputs[:byte]
@@ -66,9 +65,7 @@ end
 
 def display(outputs, flags)
   outputs.each do |key, value|
-    if flags[key]
-      print "#{value} "
-    end
+    print "#{value} " if flags[key]
   end
   puts ''
 end
@@ -80,7 +77,7 @@ end
 
 get_option_and_paths(ARGV) => { line:, word:, byte:, paths: }
 totals = Hash.new { |h, k| h[k] = 0 }
-output_flags = { line:, word:, byte:, name: true, message: true, error: true}
+output_flags = { line:, word:, byte:, name: true, message: true, error: true }
 if paths.empty?
   stdins = build_stdin_output
   display(stdins, output_flags)
