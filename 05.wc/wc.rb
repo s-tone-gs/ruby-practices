77ea30ee5576b0bf8666e24fbff6144ee9672661
shoma <s.tone@genbasupport.com>
2025-09-16 11:12:02 +0900

    今後予期せぬ副作用が起こらないように修正

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -4,7 +4,7 @@ require 'optparse'
 
 def main
   get_option_and_paths(ARGV) => { line:, word:, byte:, paths: }
-  totals = Hash.new { |h, k| h[k] = 0 }
+  totals = { line: 0, word: 0, byte: 0 }
   output_flags = { line:, word:, byte: }
   if paths.empty?
     stdins = build_stdin_output
@@ -80,18 +80,18 @@ end
 def calculate_total(totals, outputs)
   return totals if outputs.key?(:error)
 
-  totals[:line] += outputs[:line]
-  totals[:word] += outputs[:word]
-  totals[:byte] += outputs[:byte]
-  totals
+  totals.each_with_object({}) do |(key, value), hash|
+    hash[key] = value + outputs[key]
+  end
 end
 
 def display(outputs, flags)
-  # ループ処理にするとflagsとoutputsの要素の順番が同期する必要があるため、今後予期せぬバグに繋がると判断しこのように記述している。
-  outputs.delete(:line) unless flags[:line]
-  outputs.delete(:word) unless flags[:word]
-  outputs.delete(:byte) unless flags[:byte]
-  outputs.each_value do |value|
+  filtered_outputs = outputs.reject do |key|
+    (key == :line && !flags[:line]) ||
+      (key == :word && !flags[:word]) ||
+      (key == :byte && !flags[:byte])
+  end
+  filtered_outputs.each_value do |value|
     print "#{value} "
   end
   puts ''
