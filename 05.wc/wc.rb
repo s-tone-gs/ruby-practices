18cd3cffd95346306c8d214c9669abcf4b43787a
shoma <s.tone@genbasupport.com>
2025-09-24 11:13:44 +0900

    合計算出のメソッドのロジックをシンプルに修正

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -19,16 +19,16 @@ end
 def option_and_paths
   line = false
   word = false
-  byte = false
+  size = false
   opt = OptionParser.new
   opt.on('-l') { |v| line = v }
   opt.on('-w') { |v| word = v }
-  opt.on('-c') { |v| byte = v }
+  opt.on('-c') { |v| size = v }
   paths = opt.parse(ARGV)
-  if !line && !word && !byte
-    [ paths, { line: true, word: true, byte: true} ]
+  if !line && !word && !size
+    [ paths, { line: true, word: true, size: true} ]
   else
-    [ paths, { line:, word:, byte:, paths: } ]
+    [ paths, { line:, word:, size:, paths: } ]
   end
 end
 
@@ -36,7 +36,7 @@ def create_text_metadata(option_flags, input: , name: nil)
   {
     line_count: option_flags[:line] ? input.lines.count : nil,
     word_count: option_flags[:word] ? input.split.count : nil,
-    size: option_flags[:byte] ? input.size : nil,
+    size: option_flags[:size] ? input.size : nil,
     name: name
   }.compact
 end
@@ -46,20 +46,22 @@ def build_text_metadata(paths, option_flags)
     create_text_metadata(option_flags, input: File.read(path), name: path)
   end
   if text_metadata_collection.length > 1
-    text_metadata_collection + [calculate_total(text_metadata_collection)]
+    text_metadata_collection + [calculate_total(text_metadata_collection, option_flags)]
   else
     text_metadata_collection
   end
 end
 
-def calculate_total(text_metadata_collection)
-  totals = text_metadata_collection.each_with_object({}) do |text_metadata, temporary|
-    text_metadata.each do |key, data|
-      # text_metadataにはtotalを求めたい値以外も存在するためフィルタをかけている
-      temporary[key] = (temporary[key] || 0) + data if %i[line_count word_count size].include?(key)
-    end
+def calculate_total(text_metadata_collection, option_flags)
+  line_count_sum = option_flags[:line] ? 0 : nil
+  word_count_sum = option_flags[:word] ? 0 :nil
+  size_sum = option_flags[:size] ? 0 :nil
+  text_metadata_collection.each do |text_metadata|
+    line_count_sum += text_metadata[:line_count] if option_flags[:line]
+    word_count_sum += text_metadata[:word_count] if option_flags[:word]
+    size_sum += text_metadata[:size] if option_flags[:size]
   end
-  totals.merge(name: 'total')
+  { line_count: line_count_sum, word_count: word_count_sum, size: size_sum, name: 'total' }.compact
 end
 
 def calculate_output_width(text_metadata_collection)
