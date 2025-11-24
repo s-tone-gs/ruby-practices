dcee9cb4d45554d45579c3217b84f9fe01654710
shoma <s.tone@genbasupport.com>
2025-11-25 08:33:59 +0900

    shotクラスをより簡潔にするために修正

--- 06.bowling_object/shot.rb
+++ 06.bowling_object/shot.rb
@@ -4,14 +4,6 @@ class Shot
   attr_reader :score
 
   def initialize(string_score)
-    @score = to_int_score(string_score)
-  end
-
-  private
-
-  def to_int_score(string_score)
-    return 10 if string_score == 'X'
-
-    string_score.to_i
+    @score = string_score == 'X' ? 10 : string_score.to_i
   end
 end
