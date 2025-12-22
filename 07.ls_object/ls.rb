e054253255a653bcbf08ab7b6ee6027d10d163e7
shoma <s.tone@genbasupport.com>
2025-12-22 09:18:00 +0900

    配列の順番に依存しないように修正

--- 07.ls_object/ls.rb
+++ 07.ls_object/ls.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
-require_relative 'input_builder'
+require_relative 'command_line_options'
 require_relative 'file'
 require_relative 'file_list_printer'
 
 class Ls
   def self.run
-    getting_all, getting_reverse_order, show_in_long_format = CommandLineArgumentsParser.parse
-    files = FileMetadata.get_files(getting_all, getting_reverse_order)
-    FileListPrinter.run(files, show_in_long_format)
+    options = CommandLineOptions.new
+    files = FileMetadata.get_files(options.show_all?, options.show_reverse?)
+    FileListPrinter.run(files, options.show_long_format?)
   end
 end
 
